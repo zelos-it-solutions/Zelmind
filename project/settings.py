917b907200a3412686cfc453f39064d4c343f5dc
Blink17072002 <adeyemojoshua17@gmail.com>
2026-01-13 11:50:09 +0100

    Feat: Support SSL for Email

--- project/settings.py
+++ project/settings.py
@@ -226,7 +226,8 @@ TWILIO_WHATSAPP_TEMPLATE_SID = os.getenv('TWILIO_WHATSAPP_TEMPLATE_SID')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
